c6bc2ddcca23a3190a4cea2d9f36d49f4db51cc6
Mason Mathai <masonmathai@gmail.com>
2023-12-02 23:31:09 -0500

    entry page is viewable

--- components/pages/page/Page.tsx
+++ components/pages/page/Page.tsx
@@ -1,31 +1,75 @@
-import { CustomPortableText } from '@/components/shared/CustomPortableText'
-import { Header } from '@/components/shared/Header'
+import ImageBox from '@/components/shared/ImageBox'
 import { EntryPayload } from '@/types'
+import { EncodeDataAttributeCallback } from '@sanity/react-loader/rsc'
+import classNames from 'classnames'
 
 export interface PageProps {
   data: EntryPayload | null
+  encodeDataAttribute?: EncodeDataAttributeCallback
 }
 
-export function Page({ data }: PageProps) {
+export function Page({ data, encodeDataAttribute }: PageProps) {
   // Default to an empty object to allow previews on non-existent documents
-  const { title, shortDescription } = data ?? {}
+  const { title, location, date, image, shortDescription } = data ?? {}
+  const table = new Map<string, string | undefined>([
+    ['title', title],
+    ['location', location],
+    ['date', date],
+  ])
 
   return (
     <div>
       <div className="mb-14">
-        {/* Header */}
-        <Header title={title} />
-
-        {/* Body */}
-        {/* {body && (
-          <CustomPortableText
-            paragraphClasses="font-serif max-w-3xl text-gray-600 text-xl"
-            value={body}
-          />
-        )} */}
+        <div>
+          <div className="mb-14">
+            <div className="mb-14">
+              {/* Image  */}
+              <ImageBox
+                data-sanity={encodeDataAttribute?.('image')}
+                image={image}
+                // @TODO add alt field in schema
+                alt=""
+                classesWrapper="relative aspect-[16/9]"
+              />
+            </div>
+            <div className="flex flex-col w-[20rem]">
+              {Array.from(table).map(([key, value], index) => {
+                if (!value) return null
+                return (
+                  <p
+                    key={key}
+                    className={classNames(
+                      getTableElementStyle(index, table.size, true),
+                      'pl-2',
+                    )}
+                  >
+                    {`${key}: ${value}`}
+                  </p>
+                )
+              })}
+              {/* <ArrowNav entries={entriesData!} /> */}
+            </div>
+          </div>
+        </div>
       </div>
       <div className="absolute left-0 w-screen border-t" />
     </div>
+  )
+}
+export function getTableElementStyle(
+  index: number,
+  length: number,
+  columnPriority: boolean = false,
+) {
+  return classNames(
+    'border border-black',
+    columnPriority
+      ? index === length - 1
+        ? 'border-b-[1px]'
+        : 'border-b-0'
+      : index === length - 1
+        ? 'max-lg:border-b-[1px] lg:border-r-[1px]'
+        : 'max-lg:border-b-0 lg:border-r-0',
   )
 }
 
