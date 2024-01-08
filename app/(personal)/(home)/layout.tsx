2d9bbbd1a6d2a89c862979d5efd2dbe8e0b67a5f
Mason Mathai <masonmathai@gmail.com>
2024-01-07 20:09:22 -0500

    Got rid of everything on mobile

--- app/(personal)/(home)/layout.tsx
+++ app/(personal)/(home)/layout.tsx
@@ -9,6 +9,7 @@ import { Navbar } from '@/components/shared/Navbar'
 import { urlForOpenGraphImage } from '@/sanity/lib/utils'
 import { getAllEntries, getSettings } from '@/sanity/loader/loadQuery'
 import Loading from './loading'
+import Delayed from '@/components/shared/Delayed'
 
 const VisualEditing = dynamic(() => import('@/sanity/loader/VisualEditing'))
 
@@ -45,20 +46,17 @@ export default async function IndexRoute({
   children: React.ReactNode
 }) {
   return (
-    <>
+    <Suspense fallback={<Loading />}>
       <div className="flex min-h-screen flex-col text-black">
-        <h1 className="mt-3 w-screen text-center text-2xl">Mason Mathai</h1>
         {/* <div className="items-center sm:flex sm:justify-between">
           <Suspense>
             <Navbar />
           </Suspense>
           <h1 className="hidden sm:block">Mason Mathai</h1>
         </div> */}
-        <div className="mt-5 flex-grow px-4 md:px-16 lg:px-32">
-          <Suspense fallback={<Loading />}>{children}</Suspense>
-        </div>
+        <div className="mt-5 flex-grow px-4 md:px-16 lg:px-32">{children}</div>
       </div>
       {draftMode().isEnabled && <VisualEditing />}
-    </>
+    </Suspense>
   )
 }
